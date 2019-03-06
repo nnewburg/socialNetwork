7ebfab9c403996382f0e0fe618adc0894a8270a2
Nick <pickme1152019@gmail.com>
2019-03-06 23:08:14 +0000

    prints out the users name and who follows them

--- socialNetwork.js
+++ socialNetwork.js
@@ -30,3 +30,30 @@ var data = {
     follows: ["f05"]
   }
 };
+
+//write a function to:
+//List everyone and for each of them, list the names of who they follow and who follows them
+
+function listUsers(){
+
+  //create a for in loop to list each user
+  for(let user in data){
+    //prints the users name
+    console.log(data[user]["name"]);
+    //create an empty array to store a list of names
+    let listOfFol = [];
+      //a for loop running through a users follows array
+      for (let i = 0; i < data[user]["follows"].length; i++){
+        //new variable to store the id of who they follow
+        let idUser = data[user]["follows"][i]
+        //add the actual name of the user to the empty array
+        listOfFol.push(data[idUser]["name"]);
+      }
+    //print the people they follow
+    console.log(`${data[user]["name"]} follows ${listOfFol}`);
+  }
+
+
+}
+
+listUsers();
